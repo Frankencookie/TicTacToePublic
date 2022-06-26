1b33f37a6713592add1db9a43080a52c0a11bba4
Nick <frankencookie@btinternet.com>
2022-06-26 13:09:27 +0100

    Victory bug fix

--- TicTacToe/TicTacToe/GameManager.cpp
+++ TicTacToe/TicTacToe/GameManager.cpp
@@ -53,8 +53,12 @@ bool GameManager::CheckVictory()
     //ditto for 3
     //Diagonal 3 in a row
 
+    //This could be made more efficient by only testing squares next to the most recently placed square
+    //No point in testing every square
+    //This method can find if there's a victory overall so may be useful anyway, might make a second method for testing victory by square
+
     //Row test
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 1; j++)
         {
@@ -66,7 +70,7 @@ bool GameManager::CheckVictory()
     }
 
     //column test
-    for (int j = 0; j < 2; j++)
+    for (int j = 0; j < 3; j++)
     {
         for (int i = 0; i < 1; i++)
         {
@@ -76,6 +80,20 @@ bool GameManager::CheckVictory()
             }
         }
     }
+
+    //Diagonal tests
+    //00 01 02
+    //10 11 12
+    //20 21 22
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != 0)
+    {
+        return true;
+    }
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != 0)
+    {
+        return true;
+    }
+
 
     return false;
 }
